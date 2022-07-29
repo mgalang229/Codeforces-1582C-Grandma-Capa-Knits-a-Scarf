c27ce99940acd734c26371330b45e33388b6d98f
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2022-07-29 13:39:53 +0800

    Create sol.cpp

---
+++ sol.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//bool is_palindrome(string word) {
+	//int n = word.size();
+	//for (int i = 0; i < n / 2; i++) {
+		//if (word[i] != word[n-1-i]) {
+			//return false;
+		//}
+	//}
+	//return true;
+//}
+
+bool is_present(char letter, string s) {
+	for (int i = 0; i < (int) s.size(); i++) {
+		if (letter == s[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+void test_cases() {
+	int n;
+	string s;
+	cin >> n >> s;
+	int mn = 1e5 + 1;
+	for (int i = 0; i < 26; i++) {
+		char letter = 'a' + i;
+		if (is_present(letter, s)) {
+			//string word = "";
+			//for (int j = 0; j < n; j++) {
+				//if (s[j] != letter) {
+					//word += s[j];
+				//}
+			//}
+			int cnt = 0;
+			int from = 0, to = n - 1;
+			bool valid = true;
+			while (from <= to) {
+				if (s[from] == s[to]) {
+					from++;
+					to--;
+				} else {
+					if (s[from] == letter) {
+						from++;
+						cnt++;
+					} else if (s[to] == letter) {
+						to--;
+						cnt++;
+					} else {
+						valid = false;
+						break;
+					}
+				}
+			}
+			if (valid) {
+				mn = min(mn, cnt);
+			}
+		}
+	}
+	cout << (mn != (1e5 + 1) ? mn : -1) << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int T = 1;
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++) {
+		test_cases();
+	}
+}
